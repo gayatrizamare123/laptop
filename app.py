0de201e07aee1dde4b41a7fcf8c66053f1146320
GAYATRI GAJANAN ZAMARE <97080861+gayatrizamare123@users.noreply.github.com>
2023-04-24 00:30:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import pickle4 as pickle
 import numpy as np
 
 # import the model
-pipe = pickle.load(open('laptop/pipe.pkl','rb'))
+ with open('pipe.pkl', 'rb') as f:
+       pipe = pickle.load(f) 
+# pipe = pickle.load(open('laptop/pipe.pkl','rb'))
 df = pickle.load(open('laptop/df.pkl','rb'))
 
 st.title("Laptop Predictor")
